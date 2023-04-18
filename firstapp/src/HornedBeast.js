65c89ec42c99aad56485d982a1155903c54d48a6
Jayden Boyd <jaydenboydb1@gmail.com>
2023-04-18 15:41:40 -0500

    Finished da Lab

--- firstapp/src/HornedBeast.js
+++ firstapp/src/HornedBeast.js
@@ -7,7 +7,8 @@ function HornedBeast(props){
             <h2>
                 {props.title}
             </h2>
-            <img src={props.imageUrl} alt={props.keyword} title={props.title} width='550px'/>
+            <img src={props.imageUrl} alt={props.keyword} title={props.title} width='550px'/> 
+            {/* formatting the picture size */}
             
             <p>
             {props.description}
