1bbc6aec2756f4e9b90b6acb3d3f5fb1180b5216
Jayden Boyd <jaydenboydb1@gmail.com>
2023-05-03 09:14:52 -0500

    Got the filter method right ooooh weeee

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -6,30 +6,45 @@ import Main from './Main';
 import beastGallery from './data.json'
 import { useState } from 'react';
 import { Modal, Button } from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';  
-import Form from 'react-bootstrap/Form' 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form'
 function App() {
+  // let choice = "Two"
   const [show, setShow] = useState(true)
   const [displaySelectedBeasts, setDisplaySelectedBeasts] = useState(false)
   const [currentSelectedBeasts, setCurrentSelectedBeasts] = useState(undefined)
-  const [filter, filterFunction] = useState(undefined)
+  const [filter, filterFunction] = useState("")
   return (
     <div className="App">
+      <Form.Select onChange={function(event){
+        filterFunction(event.target.value  )
+      }}aria-label="Default select example">
+
+       
+        <option>Open this select menu</option>
+        <option value="One">One</option>
+        <option value="Two">Two</option>
+        <option value="Three">Three</option>
+      </Form.Select>
+      <header className='App-Header'>
+      <h1>{filter}</h1>
+
       <Modal show={show} onHide={function () {
         setShow(false)
       }}>
-        
+
         <img src={displaySelectedBeasts} alt=''></img>
         <h3>{currentSelectedBeasts}</h3>
-      </Modal> 
+      </Modal>
       {/* <Button onClick={function(){
         setShow(true)
       }
       }>
       Launch</Button> */}
       <Header />
-      <Main  filterFunction={filterFunction} setShow={setShow} setCurrentSelectedBeasts={setCurrentSelectedBeasts} setDisplaySelectedBeasts={setDisplaySelectedBeasts}/>
+      <Main filterFunction={filterFunction} setShow={setShow} setCurrentSelectedBeasts={setCurrentSelectedBeasts} setDisplaySelectedBeasts={setDisplaySelectedBeasts} />
       <Footer />
+      </header>
     </div>
 
   );
--- firstapp/src/Main.js
+++ firstapp/src/Main.js
@@ -2,28 +2,33 @@ import React, { useState } from 'react';
 import HornedBeast from './HornedBeast';
 import beastGallery from './data.json'
 import { Button } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form'
 // Importing all the goods  from where they need to be, specifically beastGallery, an array of objects so i don't have to display them one by one.
 function Main(props) {
     const [imageLiked, setImageLikes] = useState(false)
     const [likes, setLikes] = useState(0)
     let heart = '🖤'
     heart = imageLiked ? '❤️' : heart
-    
+
+    let filteredBeastGallery = beastGallery.filter((beast) => beast.horns === 1);
+
     function beastMap(value,index){
         console.log("Log",value)
-        return <HornedBeast filterFunction={props.filterFunction} beastGallery={value} setDisplaySelectedBeasts={props.setDisplaySelectedBeasts} setCurrentSelectedBeasts={props.setCurrentSelectedBeasts} setShow={props.setShow} imageUrl ={value.image_url} description ={value.description} keyword ={value.keyword}  />
+        return <HornedBeast filterFunction={props.filterFunction} beast={value} setDisplaySelectedBeasts={props.setDisplaySelectedBeasts} setCurrentSelectedBeasts={props.setCurrentSelectedBeasts} setShow={props.setShow} imageUrl ={value.image_url} description ={value.description} keyword ={value.keyword}  />
     }
     return (
         <main style={{ border: "8px solid green" }}>
-                   {/* <Button onClick= {function(){
-                    props.setShow(true)
-                   }}>Launch Modal</Button> */}
-            {/* <HornedBeast imageUrl="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80" title="Rhino Family" description="Parent rhino with two babies" keyword="rhino" />
 
-            <HornedBeast imageUrl="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg" title="Unicorn Head" description="Someone wearing a very silly unicorn head mask" keyword="unicorn" /> */}
-        
+                  <Form.Select onChange={function(event){
+        props.filterFunction(event.target.value)
+      }}aria-label="Default select example">
+
+        <option>Open this select menu</option>
+        <option value="One">One</option>
+        <option value="Two">Two</option>
+        <option value="Three">Three</option>
+      </Form.Select>
             {beastGallery.map(beastMap)}
-
             {/* This where the money being made right here, this both of the images and they are being formatted somewhere else follow me */}
         </main>
         
